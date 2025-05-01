5cee41e46fc5794f130c7e2ac66d0f6348952b0e
Jessica Ye <ssicaj00@gmail.com>
2025-05-01 18:54:01 -0400

    change in app.js with public url?

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from './Footer';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
